b5c0f84abd85fbc95e076063ccbb632d6db5dafa
@Mikist <mikeochango3@gmail.com>
2024-10-19 02:10:56 +0300

    Let's Build Your Database: Your Gateway to Data Adventure!

--- MySQLServer.py
+++ MySQLServer.py
@@ -2,11 +2,25 @@ import mysql.connector
 import pymysql
 from mysql.connector import errors
 
+# try:
+#         conn = pymysql.connect(
+#         host="localhost",
+#         user="root",
+#         password="@Mikist@23.@Mikist@23."
+#     )
+# except pymysql.MySQLError as err:
+#         print(f"Error: {err}")
+
+# cursor = conn.cursor()
+# cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+# print(f"Database 'alx_book_store' created successfully!") 
+
 try:
-        conn = pymysql.connect(
+        conn = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="@Mikist@23.@Mikist@23."
+        password="@Mikist@23.@Mikist@23.",
+        auth_plugin='mysql_native_password'
     )
 except pymysql.MySQLError as err:
         print(f"Error: {err}")
