8f53ec1975aeb21ac153c2c0f76516f63c619fa5
@Mikist <mikeochango3@gmail.com>
2024-10-19 01:36:33 +0300

    A Magical Database for Your Dream Online Bookstore Adventure!

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -12,25 +12,25 @@ publication_date DATE
 Authors: Stores information about authors.
 
 CREATE TABLE IF NOT EXISTS Authors
-author_id (Primary Key)
+author_id (PRIMARY KEY)
 author_name VARCHAR(215)
 
 Customers: Stores information about customers.
 CREATE TABLE IF NOT EXISTS Customers
-customer_id (Primary Key)
+customer_id (PRIMARY KEY)
 customer_name VARCHAR(215)
 email VARCHAR(215)
 address TEXT
 
 Orders: Stores information about orders placed by customers.
 CREATE TABLE IF NOT EXISTS Orders
-order_id (Primary Key)
-customer_id (Foreign Key referencing Customers table)
+order_id (PRIMARY KEY)
+customer_id (FOREIGN KEY referencing Customers table)
 order_date DATE
 
 Order_Details: Stores information about the books included in each order.
 CREATE TABLE IF NOT EXISTS Order_Details
-orderdetailid (Primary Key)
-order_id (Foreign Key referencing Orders table)
-book_id (Foreign Key referencing Books table)
+orderdetailid (PRIMARY KEY)
+order_id (FOREIGN KEY referencing Orders table)
+book_id (FOREIGN KEY referencing Books table)
 quantity DOUBLE
